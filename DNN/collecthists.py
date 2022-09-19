1e57affc115d7fa50ed2f74565bdf0b0caf3b2d5
yeonsu108 <yeonsu.ryou@gmail.com>
2022-09-19 15:27:55 +0900

    update systs in collecthists.py

--- DNN/collecthists.py
+++ DNN/collecthists.py
@@ -2,7 +2,7 @@ import ROOT
 import os
 import sys
 
-projname = "mar_02"
+projname = "aug22"
 #projname = "test"
 label = "optimized"
 label = "rerun"
@@ -13,7 +13,7 @@ hists = ["data_obs","hstacked_mc_h_dnn_pred",
         "LFV_TTc_s","LFV_TTc_v","LFV_TTc_t","LFV_TTu_s","LFV_TTu_v","LFV_TTu_t",
         "TT_di","TT_semi","ST","Wjets","Others"]
 
-systs = {"norm":"norm",
+systs = {"nom":"nom",
         "jesup":"jesUp","jesdown":"jesDown",
         "puup":"puUp","pudown":"puDown",
         "btagup_jes":"btag_jesUp","btagdown_jes":"btag_jesDown",
@@ -21,11 +21,34 @@ systs = {"norm":"norm",
         "btagup_lf":"btag_lfUp","btagdown_lf":"btag_lfDown",
         }
 
+systs = {"nom":"nom",
+        "puup":"puUp","pudown":"puDown",
+        "btagup_hf":"btag_hfUp","btagdown_hf":"btag_hfDown",
+        "btagup_lf":"btag_lfUp","btagdown_lf":"btag_lfDown",
+        "btagup_hfstats1":"btag_hfstats1Up","btagdown_hfstats1":"btag_hfstats1Down",
+        "btagup_lfstats1":"btag_lfstats1Up","btagdown_lfstats1":"btag_lfstats1Down",
+        "btagup_hfstats2":"btag_hfstats2Up","btagdown_hfstats2":"btag_hfstats2Down",
+        "btagup_lfstats2":"btag_lfstats2Up","btagdown_lfstats2":"btag_lfstats2Down",
+        "btagup_cferr1":"btag_cferr1Up","btagdown_cferr1":"btag_cferr1Down",
+        "btagup_cferr2":"btag_cferr2Up","btagdown_cferr2":"btag_cferr2Down",
+
+        "up_jesAbsolute":"jesAbsoluteUp","down_jesAbsolute":"jesAbsoluteDown",
+        "up_jesAbsolute_year":"jesAbsolute_yearUp","down_jesAbsolute_year":"jesAbsolute_yearDown",
+        "up_jesBBEC1":"jesBBEC1Up","down_jesBBEC1":"jesBBEC1Down",
+        "up_jesBBEC1_year":"jesBBEC1_yearUp","down_jesBBEC1_year":"jesBBEC1_yearDown",
+        "up_jesEC2":"jesEC2Up","down_jesEC2":"jesEC2Down",
+        "up_jesEC2_year":"jesEC2_yearUp","down_jesEC2_year":"jesEC2_yearDown",
+        "up_jesFlavorQCD":"jesFlavorQCDUp","down_jesFlavorQCD":"jesFlavorQCDDown",
+        "up_jesRelativeBal":"jesRelativeBalUp","down_jesRelativeBal":"jesRelativeBalDown",
+        "up_jesRelativeSample_year":"jesRelativeSample_yearUp","down_jesRelativeSample_year":"jesRelativeSample_yearDown",
+        }
+
 #systs = {"norm":"norm",
 #        "jesup":"jesUp","jesdown":"jesDown",
 #        "puup":"puUp","pudown":"puDown",
 #        }
-runs = ["run16APV","run16","run17","run18","run2"]
+runs = ["run16APV","run16","run17","run18"]#,"run2"]
+
 outfolder = "./pred_"+projname
 
 if not os.path.isdir(outfolder):
@@ -51,7 +74,7 @@ for run in runs:
             outfname = outfolder+"/pred_"+run+"_cat2.root"
         outf = ROOT.TFile(outfname,"RECREATE")
         for key, value in systs.items():
-            infilename = label+"_"+lfvproc+projname+"/"+key+"/pred_hists/noblind/"+rname
+            infilename = label+"_"+projname+"_"+lfvproc.lower()+"lfv/"+key+"/pred_hists/noblind/"+rname
             inf = ROOT.TFile(infilename)
             outf.cd()
             for h in hists:
@@ -60,8 +83,10 @@ for run in runs:
                     print("wrong hist",h)
                     continue
                 copyhname = h
-                if key is not "norm":
+                if key is not "nom":
                     copyhname = h+"_"+value
+                    if "year" in key:
+                        copyhname = copyhname.replace("year", "20"+run[3:5])
                 print(copyhname)
                 tmphist_copy = tmphist.Clone(copyhname)
                 tmphist_copy.Write()
